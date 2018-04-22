d6887b23fd6bad214282b43d387e1aa49d0e6982
Šarūnas Navickas <zaibacu@users.noreply.github.com>
2018-04-22 22:37:20 +0300

    Don't use PIP imports

--- {{cookiecutter.name}}/setup.py
+++ {{cookiecutter.name}}/setup.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from setuptools import setup
-from pip.req import parse_requirements
-from pip.exceptions import InstallationError
 
 from {{cookiecutter.package}}.version import get_version
-
-try:
-    install_reqs = list(parse_requirements("requirements.txt", session={}))
-except InstallationError:
-    # There are no requirements
-    install_reqs = []
 
 setup(name="{{cookiecutter.package}}",
       version=get_version(),
